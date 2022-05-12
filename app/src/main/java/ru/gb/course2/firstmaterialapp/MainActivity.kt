5a1313e71321c4cd591025f841c6e81872ffe0ec
Alexander Voronov <dr.strum@gmail.com>
2022-05-12 17:20:08 +0300

    Initial commit

---
+++ app/src/main/java/ru/gb/course2/firstmaterialapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.gb.course2.firstmaterialapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
