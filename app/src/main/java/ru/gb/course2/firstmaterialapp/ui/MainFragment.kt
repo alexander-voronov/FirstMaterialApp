15d2613797a71457efb8c2eec1cfea15afa62af1
Alexander Voronov <dr.strum@gmail.com>
2022-05-17 18:23:21 +0300

    Added ViewModelProvider.Factory

--- app/src/main/java/ru/gb/course2/firstmaterialapp/ui/MainFragment.kt
+++ app/src/main/java/ru/gb/course2/firstmaterialapp/ui/MainFragment.kt
@@ -3,10 +3,14 @@ package ru.gb.course2.firstmaterialapp.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import ru.gb.course2.firstmaterialapp.R
 import ru.gb.course2.firstmaterialapp.databinding.FragmentMainBinding
+import ru.gb.course2.firstmaterialapp.domain.NasaRepositoryImpl
 
-class MainFragment : Fragment (R.layout.fragment_main){
+class MainFragment : Fragment(R.layout.fragment_main) {
+
+    private val viewMode: MainViewModel by viewModels { MainViewModelFactory(NasaRepositoryImpl()) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/ru/gb/course2/firstmaterialapp/ui/MainViewModel.kt
+++ app/src/main/java/ru/gb/course2/firstmaterialapp/ui/MainViewModel.kt
@@ -1,6 +1,13 @@
 package ru.gb.course2.firstmaterialapp.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ru.gb.course2.firstmaterialapp.domain.NasaRepository
 
-class MainViewModel : ViewModel() {
+class MainViewModel(val repository: NasaRepository) : ViewModel() {
+}
+
+class MainViewModelFactory(val repository: NasaRepository) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T = MainViewModel(repository) as T
 }
